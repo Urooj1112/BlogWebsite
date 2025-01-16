14062757ab834f60bc74e3f6dbb3e12ac28a6b48
Urooj Saeed <165631956+Urooj1112@users.noreply.github.com>
2025-01-16 06:28:30 -0800

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "@/app/components/Footer";
+import Header from "@/app/components/Header";
+import Hero from "@/app/components/Hero";
+export default function Home() {
+  return (
+   <div>
+<Header/>
+<Hero/>
+<Footer/>
+   </div>
+  );
+}
